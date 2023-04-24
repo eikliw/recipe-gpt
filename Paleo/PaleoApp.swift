5e2ce31ac48eef30f84d18513972699245138d08
Adam Wilkie <eikliw@gmail.com>
2023-04-24 13:09:05 -0400

    Initial Commit

---
+++ Paleo/PaleoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaleoApp.swift
+//  Paleo
+//
+//  Created by Adam Wilkie on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PaleoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
